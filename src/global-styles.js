63eb33f590fe7c444ffe15941fec0aace5013375
Jérémy Raffin <jeremy.raffin@gmail.com>
2017-06-24 15:57:50 +0200

    Styled header component

--- src/global-styles.js
+++ src/global-styles.js
@@ -2,6 +2,9 @@ import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
+    html {
+        font-size: 16px;
+    }
     body {
         font-family: 'Source Sans Pro', Helvetica, sans-serif;
         -webkit-font-smoothing: antialiased;
