9c9a5cc5cac3e0420cf2f2d903d7b1ada375351c
Jerome Raffin <jerome.raffin@ekino.com>
2017-06-24 16:41:24 +0200

    Styled card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,20 +6,97 @@ const StyledCard = styled.div`
     /* Box model */
     display: flex;
     flex-direction: column;
+    max-width: 230px;
+
+    /* Visual */
+    background-color: ${props => props.theme.grayLight};
+    border-radius: 3px;
+`;
+
+const Content = styled.div`
+    /* Box model */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: .6rem;
+`;
+
+const Title = styled.h3`
+    /* Box model */
+    margin: .6rem 0 0;
+
+    /* Typography */
+    font-size: 1.4rem;
+    font-weight: 600;
+    line-height: 1;
+`;
+
+const Link = styled.a`
+    /* Visual */
+    color: ${props => props.theme.gray};
+    text-decoration: none;
+`;
+
+const Copy = styled.p`
+    /* Box model */
+    margin: 0 0 1.2rem;
+
+    /* Typography */
+    font-size: 1rem;
+    font-weight: 300;
+
+    /* Visual */
+    color: ${props => props.theme.gray};
+`;
+
+const List = styled.ul `
+    /* Box model */
+    margin: 0;
+    padding: 0 0 .6rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    height: 60px;
+    overflow: hidden;
+
+    /* Visual */
+    list-style: none;
+`;
+
+const Item = styled.li`
+    /* Box model */
+    margin-bottom: .6rem;
+    padding: .2rem .8rem;
+    height: 20px;
+
+    /* Visual */
+    color: #ffffff;
+    background-color: ${props => props.theme.yellow};
+    border-radius: 3px;
+
+    /* Typography */
+    font-size: .8rem;
+    font-weight: 600;
+
+    &:not(:first-child) {
+        margin-left: .4rem;
+    }
 `;
 
 const Card = ({avatar_url, name, html_url, login, location, languages}) => (
   <StyledCard>
-      <img src={avatar_url} alt={name} width='180' height='180' />
-      <h2>
-          <a href={html_url}>{name ? name : login}</a>
-      </h2>
-      <p>{location}</p>
-      <ul>
-        {
-          languages.map((language, index) => (<li key={index}>{language}</li>))
-        }
-      </ul>
+      <img src={avatar_url} alt={name} width='230' height='230' />
+      <Content>
+          <Title>
+              <Link href={html_url}>{name ? name : login}</Link>
+          </Title>
+          <Copy>{location}</Copy>
+          <List>
+            {
+              languages.map((language, index) => (<Item key={index}>{language}</Item>))
+            }
+        </List>
+    </Content>
   </StyledCard>
 );
 
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,7 @@
 const theme = {
-
+    gray: '#666666',
+    grayLight: '#dddddd',
+    yellow: '#ffd300'
 };
 
 export default theme;
