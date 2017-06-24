68a2da467b153d53cc289728f04417a1f22335aa
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2017-06-24 04:08:01 +0200

    adding docstrings

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,6 +1,11 @@
 import fetch from 'isomorphic-fetch';
 
 function checkStatus(response) {
+    /**
+     * Check a fetch response to see if an http error must be raised. If not, return the response.
+     * @param {Object} response The fetch response object containing status and the response value.
+     * @returns {Object} The response value converted from JSON.
+     */
     if (response.status >= 200 && response.status < 300) {
         return response.json();
     }
@@ -10,5 +15,11 @@ function checkStatus(response) {
 }
 
 export default function fetchAPI(url, options) {
+    /**
+     * A fetch abstaction checking http status before returning the response.
+     * @param {String} url The url of the api to contact.
+     * @param {Object} options The options (header, body, mode, ...) to use.
+     * @returns {Promise} The response in a promise.
+     */
     return fetch(url, options).then(checkStatus);
 }
