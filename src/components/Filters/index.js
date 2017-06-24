307a6d5e5b052befa387bcbf7e2c4a78d1ebccec
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2017-06-24 10:24:54 +0200

    adding city completion and formatting

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import VirtualizedSelect from 'react-virtualized-select'
+import SelectPlaces from 'react-select-places'
+import { parseLocation } from '../../apis/google';
 
 const StyledFilters= styled.div`
     /* Box model */
@@ -21,16 +23,25 @@ class Filters extends Component {
         this.state = {
             filter: {
                 location: '',
-                language: ''
+                language: []
             }
         }
     }
 
-    handleChangeLanguages = value => {
+    handleChangeLanguage = value => {
         this.setState({
             filter: {
-              ...this.state.filters,
+              ...this.state.filter,
               language: value.map(({value}) => value)
+            }
+        });
+    }
+
+    handleChangeLocation = value => {
+        this.setState({
+            filter: {
+              ...this.state.filter,
+              location: parseLocation([value])
             }
         });
     }
@@ -46,9 +57,15 @@ class Filters extends Component {
         return (
             <StyledFilters>
                 <form onSubmit={this.handleSubmit}>
+                    <SelectPlaces
+                      onChange={this.handleChangeLocation}
+                      autocompletionRequest={{
+                          types: ['(cities)']
+                        }}
+                    />
                     <VirtualizedSelect
                       options={options}
-                      onChange={this.handleChangeLanguages}
+                      onChange={this.handleChangeLanguage}
                       value={filter.language}
                       multi
                     />
