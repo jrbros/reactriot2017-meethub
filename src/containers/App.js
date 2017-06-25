8a3eaf341d27ef4ade966ae3ed1f692c61535a67
Jérémy Raffin <jeremy.raffin@gmail.com>
2017-06-25 17:20:22 +0200

    Fix router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import App from '../components/App';
 import { askForGeoLocation } from '../ducks/geoLocation';
 
-export default connect(
+export default withRouter(connect(
     ({app}) => ({searchIsActive: app.searchIsActive}),
     dispatch => bindActionCreators({askForGeoLocation}, dispatch)
-)(App);
+)(App));
