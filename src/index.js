3e2151203635185d4eb9651a13b39c039cf341ba
mousnier guillaume <mousnier.guillaume@gmail.com>
2017-06-25 12:33:41 +0200

    disable service workers for the moment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 import 'normalize.css/normalize.css';
 import 'react-select/dist/react-select.css'
@@ -10,4 +10,4 @@ import 'react-virtualized-select/styles.css'
 import './global-styles';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
